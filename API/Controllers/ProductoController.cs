aef0153c973662d916182b2dadec125c60e3672a
Edwing Nicolás García C <edwingnico@gmail.com>
2023-10-20 18:39:26 -0500

    API POST GET DATABASE

---
+++ API/Controllers/ProductoController.cs
@@ -0,0 +1,78 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+
+        private readonly ApplicationDBContext _db;
+
+        public ProductoController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+
+
+
+        // GET: api/<ProductoController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<Producto> p= await _db.Producto.ToListAsync();
+            return Ok(p);
+        }
+
+
+        // GET api/<ProductoController>/5
+        [HttpGet("{IdProducto}")]
+        public async Task<IActionResult> Get(int IdProducto)
+        {
+            Producto p= await _db.Producto.FirstOrDefaultAsync(x=>x.IdProducto==IdProducto);
+            if (p == null)
+            {
+                return BadRequest();
+            }
+            return Ok(p);
+        }
+
+
+
+
+        // POST api/<ProductoController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Producto producto)
+        {
+            Producto p = await _db.Producto.FirstOrDefaultAsync(x => x.IdProducto == producto.IdProducto);
+            if(p == null && producto != null) {
+                await _db.Producto.AddAsync(producto);
+                await _db.SaveChangesAsync();
+                return Ok(producto);
+            }
+
+            return BadRequest("No existe el objeto");
+        }
+
+
+
+
+        // PUT api/<ProductoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- API/Data/ApplicationDBContext.cs
+++ API/Data/ApplicationDBContext.cs
@@ -5,7 +5,22 @@ namespace API.Data
 {
     public class ApplicationDBContext:DbContext
     {
+        
         public ApplicationDBContext( DbContextOptions<ApplicationDBContext> options ) : base( options ) { }      
-        DbSet<Producto> Producto {  get; set; }
+        public DbSet<Producto> Producto {  get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>().HasData( 
+                new Producto(){IdProducto=1,Nombre="NICO", Descripcion="LO LOGRASTE, ESTOY ORGULLOSO", Cantidad=12},
+                new Producto(){IdProducto=2,Nombre="Sebas", Descripcion="Hola", Cantidad=1},
+                new Producto(){IdProducto=5,Nombre="Rusky", Descripcion="Bai", Cantidad=22},
+                new Producto(){IdProducto=3,Nombre="Tilin", Descripcion="Setso", Cantidad=4}
+                
+                );    
+        }
+
+
+
     }
 }
