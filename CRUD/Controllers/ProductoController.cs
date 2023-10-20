e9171c0691546fde25639a4c13110afb8a951d41
Edwing Nicolás García C <edwingnico@gmail.com>
2023-10-20 17:29:28 -0500

    Vistas Create Delete e Index finalizadas

---
+++ CRUD/Controllers/ProductoController.cs
@@ -0,0 +1,57 @@
+﻿using CRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: ProductoController
+        public IActionResult Index()
+        {
+            return View(Util.Utils.ListaProducto);
+        }
+
+        // GET: ProductoController/Details/5
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductoController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public  IActionResult Create(Producto producto)
+        {
+            int id = Util.Utils.ListaProducto.Count() + 1;
+            producto.IdProducto = id;
+            Util.Utils.ListaProducto.Add(producto);
+            return RedirectToAction("Index");
+        }
+
+       
+        // GET: ProductoController/Edit/5
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+     
+
+        // GET: ProductoController/Delete/5
+        public IActionResult Delete(int IdProducto)
+        {
+            Producto p = Util.Utils.ListaProducto.Find(x => x.IdProducto == IdProducto);
+            if(p != null)
+            {
+                Util.Utils.ListaProducto.Remove(p);
+            }
+            return RedirectToAction("Index");
+        }
+
+  
+    }
+}
---
+++ CRUD/Util/Utils.cs
@@ -0,0 +1,19 @@
+﻿using CRUD.Models;
+
+namespace CRUD.Util
+{
+    public class Utils
+    {
+        public static List<Producto> ListaProducto = new List<Producto>()
+        {
+            new Producto()
+            {
+                IdProducto = 1,
+                Nombre="P",
+                Descripcion="D",
+                Cantidad=1
+            }
+        };
+
+    }
+}
